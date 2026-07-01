0ab1168d670c2d03362b9ac7800a9836d1bebbef
EvaNinou <eua.ninou@gmail.com>
2026-07-01 23:08:13 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -81,6 +81,7 @@ const [newQuote, setNewQuote] = useState({
     const { data } = await supabase.from('inventory').select('*');
     setInventory(data || []);
     }
+  
     async function loadQuotes() {
   const { data } = await supabase.from('quotes').select('*');
   setQuotes(data || []);
