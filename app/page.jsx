2f146905feb0b83a7e711e8fe7e2904c4626f328
EvaNinou <eua.ninou@gmail.com>
2026-07-02 17:49:24 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -10,6 +10,7 @@ export default function Home() {
   const [customers, setCustomers] = useState([]);
   const [projects, setProjects] = useState([]);
   const [selectedProject, setSelectedProject] = useState(null);
+  const [selectedQuote, setSelectedQuote] = useState(null);
   const [payments, setPayments] = useState([]);
   const [expenses, setExpenses] = useState([]);
   const [inventory, setInventory] = useState([]);
